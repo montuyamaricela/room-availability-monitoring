8809942fde23028e045247c7e4f3a6469167a996
Maricel A. Montuya <76860838+montuyamaricela@users.noreply.github.com>
2024-07-15 20:03:32 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
 
 export default config;
