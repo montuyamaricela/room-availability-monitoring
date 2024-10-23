fd12a5151b5a607c8edb7be93ea5d22bf3834cc7
Maricel A. Montuya <76860838+montuyamaricela@users.noreply.github.com>
2024-10-23 12:42:20 +0800

    Update account management page

--- src/app/(pages)/admin/account-management/page.tsx
+++ src/app/(pages)/admin/account-management/page.tsx
@@ -5,7 +5,7 @@ import NotAllowed from "~/components/common/NotAllowed";
 
 export default function Page() {
   const session = useSession();
-  if (session.data?.user.role != "Super Admin" || session.data?.user.role != "Admin") {
+  if (session.data?.user.role === "Security Guard" || session.data?.user.role === "Room Viewer") {
     return <NotAllowed />;
   }
 
